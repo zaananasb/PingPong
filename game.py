a6a0595a05688f531d2be962271e56c9c4f19756
zaananasb <130475311+zaananasb@users.noreply.github.com>
2024-04-16 16:03:15 +0300

    ВЕРСИЯ 0.5.0

--- game.py
+++ game.py
@@ -2,17 +2,11 @@
 
 from pygame import *
 from random import randint
-import time as time1
 clock = time.Clock()
-win_width = 700
+win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
-while True:
-    a = randint(0,255)
-    b = randint(0,255)
-    c = randint(0, 255)
-    time1.sleep(1)
-back = (a, b, c)
+back = (200, 255, 255)
 window.fill(back)
 display.set_caption("Maze")
 game = True
@@ -27,17 +21,19 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-class Enemy(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+'''class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
         global proshli
@@ -45,10 +41,19 @@ class Enemy(GameSprite):
             self.rect.y = 0
             self.rect.x = randint(80, 620)
             self.speed = randint(1,7)
-            proshli += 1
+            proshli += 1'''
+racket1 = Player('racket.png', 30, 200, 50, 150, 4) 
+racket2 = Player('racket.png', 520, 200, 50, 150, 4)
 while game:
+    window.fill(back)
+    racket1.update_l()
+    racket2.update_r()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+    racket1.reset()
+    racket2.reset()
+
     display.update()
     clock.tick(60)
